9e0f2ad8d76a2b46cf7c09f9076bb703501ff88d
sin <sinoory@126.com>
2014-02-04 15:15:27 +0800

    shop:try add template

--- shoplist/views.py
+++ shoplist/views.py
@@ -9,5 +9,11 @@ from django import forms
 from django.utils import simplejson as json
 
 def index(request):
-    print "shoplist index in..."
-    return HttpResponse("shoplist in")
+    print "shoplist index in..." 
+    try:
+        template = loader.get_template('shoplist/51.html')
+        context = Context({})
+        return HttpResponse(template.render(context))
+    except Exception,ex:
+        print ex
+    #return HttpResponse("shoplist in")
