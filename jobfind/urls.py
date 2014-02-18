465739ff535a610559d05e59b1743d5f570e2533
sin <sinoory@126.com>
2014-02-18 21:55:30 +0800

    job:remember search key in q.html when querry

--- jobfind/urls.py
+++ jobfind/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, url
 
 from jobfind import views
-
+from django.contrib.auth.views import login, logout
 urlpatterns = patterns('',
     url(r'^index$', views.index, name='index'),
     url(r'^querry$', views.querry, name='querry'),
@@ -9,4 +9,6 @@ urlpatterns = patterns('',
     url(r'^modify/(?P<rcdid>\d+)/$', views.modify),
     url(r'^submitstatus$', views.submitstatus),
     url(r'^viewljobs$', views.viewljobs),#view local jobs whith get or water status
+    url(r'^accounts/login/$',  login),
+    url(r'^accounts/logout/$', logout),
     )
